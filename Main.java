e00e96dacdb9185ea1268c63a120ecc718ef15fb
Aleksandra Wojcik <olawujcik@wp.pl>
2024-05-05 14:47:59 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 import company.implementations.EmailMessenger;
 import company.interfaces.Messenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        int result = MathUtils.add(7, 3);
+        System.out.println("Wynik dodawania: " + result);
+
         Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("Wiadomość email.");
+        messenger.sendMessage("Wynik dodawania to: " + result);
     }
 }
