835fd334f6a96841d7ff97d84c76d8b885241ae4
Aleksandra Wojcik <olawujcik@wp.pl>
2024-05-05 14:39:42 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,14 +1,9 @@
-import company.beans.Person;
-import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Person person = new Person("Ola", 23);
-            System.out.println("Name: " + person.getName());
-            System.out.println("Age: " + person.getAge());
-        } catch (InvalidAgeException e) {
-            System.out.println("Invalid age encountered: " + e.getMessage());
-        }
+        Messenger messenger = new EmailMessenger();
+        messenger.sendMessage("Wiadomość email.");
     }
 }
